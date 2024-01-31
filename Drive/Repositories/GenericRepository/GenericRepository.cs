9dd4149644fcc6ad60c23cf104a3cc987ecdcfd9
TeodorL2 <151771015+TeodorL2@users.noreply.github.com>
2024-01-31 16:31:07 +0200

    generic and user repositories

---
+++ Drive/Repositories/GenericRepository/GenericRepository.cs
@@ -0,0 +1,86 @@
+﻿using Drive.Data;
+using Drive.Data.Models.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Drive.Repositories.GenericRepository
+{
+    public class GenericRepository<TEntity>: IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly DriveContext _driveContext;
+        protected readonly DbSet<TEntity> _table;
+
+        public GenericRepository(DriveContext driveContext)
+        {
+            _driveContext = driveContext;
+            _table = _driveContext.Set<TEntity>();
+        }
+
+        // Get all
+        public async Task<List<TEntity>> GetAll()
+        {
+            return await _table.AsNoTracking().ToListAsync();
+        }
+
+        // Create
+        public void Create(TEntity entity)
+        {
+            _table.Add(entity);
+        }
+        public async Task CreateAsync(TEntity entity)
+        {
+            await _table.AddAsync(entity);
+        }
+
+        public void CreateRange(IEnumerable<TEntity> entities)
+        {
+            _table.AddRange(entities);
+        }
+
+        public async Task CreateRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _table.AddRangeAsync(entities);
+        }
+
+        // Update
+        public void Update(TEntity entity)
+        {
+            _table.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            _table.UpdateRange(entities);
+        }
+
+        // Delete
+        public void Delete(TEntity entity)
+        {
+            _table.Remove(entity);
+        }
+        public void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            _table.RemoveRange(entities);
+        }
+
+        // Find
+        public TEntity FindById(Guid id)
+        {
+            return _table.Find(id);
+        }
+
+        public async Task<TEntity> FindByIdAsync(Guid id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+        // Save
+        public bool Save()
+        {
+            return _driveContext.SaveChanges() > 0;
+        }
+        public async Task<bool> SaveAsync()
+        {
+            return await _driveContext.SaveChangesAsync() > 0;
+        }
+    }
+}
